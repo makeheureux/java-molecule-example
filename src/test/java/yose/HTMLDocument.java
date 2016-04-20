92fcaa4b32ef7d8e975cfa9cab0187ac073b57fc
Sylvain Camus <sylvain1.camus@orange.com>
2016-04-20 16:49:55 +0200

    Share

---
+++ src/test/java/yose/HTMLDocument.java
@@ -0,0 +1,27 @@
+package yose;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import com.sun.org.apache.xerces.internal.parsers.DOMParser;
+
+public final class HTMLDocument {
+
+	private HTMLDocument() {
+	}
+
+	public static Document from(String dom) throws IOException, SAXException {
+		DOMParser parser = new DOMParser();
+		parser.parse(new InputSource(new StringReader(dom)));
+		return parser.getDocument();
+	}
+
+	public static Element toElement(String dom) throws IOException, SAXException {
+		return from(dom).getDocumentElement();
+	}
+}
--- src/test/java/yose/worlds/StartWorld.java
+++ src/test/java/yose/worlds/StartWorld.java
@@ -1,44 +1,66 @@
 package yose.worlds;
 
-import com.vtence.molecule.testing.http.HttpRequest;
-import com.vtence.molecule.testing.http.HttpResponse;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import yose.YoseDriver;
+import static com.vtence.molecule.testing.http.HttpResponseAssert.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.io.IOException;
 
-import static com.vtence.molecule.testing.http.HttpResponseAssert.assertThat;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import com.vtence.molecule.testing.http.HttpRequest;
+import com.vtence.molecule.testing.http.HttpResponse;
+
+import yose.HTMLDocument;
+import yose.YoseDriver;
 
 public class StartWorld {
 
-    YoseDriver yose = new YoseDriver(9999);
+	YoseDriver yose = new YoseDriver(9999);
 
-    HttpRequest request = new HttpRequest(9999);
-    HttpResponse response;
+	HttpRequest request = new HttpRequest(9999);
+	HttpResponse response;
 
-    @Before
-    public void startGame() throws Exception {
-        yose.start();
-    }
+	@Before
+	public void startGame() throws Exception {
+		yose.start();
+	}
 
-    @After
-    public void stopGame() throws Exception {
-        yose.stop();
-    }
+	@After
+	public void stopGame() throws Exception {
+		yose.stop();
+	}
 
-    @Test
-    public void firstWebPageChallenge() throws IOException {
-        yose.home().displaysGreeting("Hello Yose");
-    }
+	@Test
+	public void firstWebPageChallenge() throws IOException {
+		yose.home().displaysGreeting("Hello Yose");
+	}
 
-    @Test
-    public void firstWebServiceChallenge() throws IOException {
-        response = request.get("/ping");
+	@Test
+	public void firstWebServiceChallenge() throws IOException {
+		response = request.get("/ping");
 
-        assertThat(response).isOK()
-                            .hasContentType("application/json")
-                            .hasBodyText("{\"alive\":true}");
-    }
+		assertThat(response).isOK().hasContentType("application/json").hasBodyText("{\"alive\":true}");
+	}
+
+	// @Test
+	public void shareChallengeLink() throws Exception {
+		response = request.get("/");
+		assertThat(response).isOK();
+		Document doc = HTMLDocument.from(response.bodyText());
+		NodeList a = doc.getElementsByTagName("a");// ("repository-link");
+		System.out.println(a.getLength());
+		Element link = doc.getElementById("repository-link");
+		System.out.println(link);
+		assertNotNull(link);
+		assertEquals(link.getTagName(), "a");
+		String target = link.getTextContent();
+		assertNotEquals(target, "");
+	}
 }
